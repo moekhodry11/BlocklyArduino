9a4ee12b3d2fed2cd8fe2163474ead2b508d037c
moekhodry11 <86708003+moekhodry11@users.noreply.github.com>
2024-05-09 17:03:06 +0300

    tmp36 code fix

--- blockly/generators/arduino/tmp36.js
+++ blockly/generators/arduino/tmp36.js
@@ -2,13 +2,10 @@
 goog.provide("Blockly.Arduino.tmp36.js");
 goog.require("Blockly.Arduino");
 
-
-
-Blockly.Arduino.tmp36_read = function() {
-  var pin = this.getFieldValue('PIN');
-  var code = 'readTMP36(' + pin + ')';
-  Blockly.Arduino.definitions_['define_tmp36'] = 'float readTMP36(int pin) {\n  int val = analogRead(pin);\n  float mv = ( val/1024.0) * 5000;\n  float cel = mv / 10;\n  return cel;\n}\n';
+Blockly.Arduino.tmp36_read = function () {
+  var pin = this.getFieldValue("PIN");
+  var code = "readTMP36(" + pin + ")";
+  Blockly.Arduino.definitions_["define_tmp36"] =
+    "float readTMP36(int pin) {\n  int val = analogRead(pin);\n  float mv = val * (5.0 / 1024.0);\n  float cel = (mv - 0.5)*100;\n  return cel;\n}\n";
   return [code, Blockly.Arduino.ORDER_ATOMIC];
-}
-
-
+};
